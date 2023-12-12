80bc694e8c61b6d0b690f87f37d497fc9dcc606f
deepennyway <79524310+deepennyway@users.noreply.github.com>
2023-12-12 11:38:23 -0500

    adding an art gallery

--- _src/_data/products.js
+++
@@ -1,7 +0,0 @@
-const { client } = require("../lib/commerce");
-
-module.exports = async () => {
-  const { data: products } = await client.products.list();
-
-  return products;
-};
--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,9 @@
+const yaml = require("js-yaml");
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("_src/css");
     eleventyConfig.addPassthroughCopy("_src/fonts");
     eleventyConfig.addPassthroughCopy("_src/img");
+    eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
     // Return your Object options:
     return {
       dir: {
